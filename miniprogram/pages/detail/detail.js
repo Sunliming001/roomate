9f182e90736f22f958761693eeaa5329c529b4d6
Sunliming001 <768891935@qq.com>
2025-12-02 20:26:44 +0800

    新增云图

--- miniprogram/pages/detail/detail.js
+++ miniprogram/pages/detail/detail.js
@@ -23,6 +23,47 @@ Page({
     this.loadRoomDetail();
   },
 
+  // --- 空函数：用于阻止事件冒泡 ---
+  preventBubble() {
+    // 这是一个空函数，专门用于 catchtap
+  },
+
+  onShareAppMessage() {
+    const title = `【友邻找室友】${this.data.info.community} 招室友啦！`;
+    const imageUrl = this.data.info.cover;
+    return {
+      title: title,
+      path: `/pages/detail/detail?id=${this.roomId}`,
+      imageUrl: imageUrl
+    }
+  },
+
+  // --- 分享弹窗控制 ---
+  openShareModal() { this.setData({ showShareModal: true }); },
+  closeShareModal() { this.setData({ showShareModal: false }); },
+
+  handleShareTo(e) {
+    const appName = e.currentTarget.dataset.app;
+    const content = `【友邻】${this.data.info.community} 招室友！pages/detail/detail?id=${this.roomId}`;
+    wx.setClipboardData({
+      data: content,
+      success: () => {
+        this.closeShareModal();
+        wx.showModal({ title: '口令已复制', content: `请打开 ${appName} 粘贴`, showCancel: false });
+      }
+    });
+  },
+
+  handleCopyLink() {
+    wx.setClipboardData({
+      data: `pages/detail/detail?id=${this.roomId}`,
+      success: () => {
+        this.closeShareModal();
+        wx.showToast({ title: '已复制' });
+      }
+    });
+  },
+
   onPullDownRefresh() {
     this.loadRoomDetail(() => {
       this.checkFavStatus();
@@ -34,8 +75,6 @@ Page({
     db.collection('rooms').doc(this.roomId).get({
       success: res => {
         const d = res.data;
-        // 确保 memberIds 存在 (兼容旧数据)
-        if (!d.memberIds) d.memberIds = [];
         this.processData(d);
         if(cb) cb();
       },
@@ -117,52 +156,32 @@ Page({
     }
   },
 
-  // --- 核心修复：聊天群组逻辑 ---
   handleChat() {
     if (!this.data.userInfo) return wx.navigateTo({ url: '/pages/login/login' });
-    
     const me = this.data.userInfo;
     const owner = this.data.info.publisher;
-    const roommates = this.data.info.memberIds || [];
-
-    // 1. 组装群成员：房主 + 我 + 所有已入住室友
-    // 使用 Set 去重 (防止我自己也是室友，或者房主也是室友导致ID重复)
-    const memberSet = new Set([owner._id, me._id, ...roommates]);
-    const allMembers = Array.from(memberSet);
+    
+    // 自己不能跟自己聊
+    if(me._id === owner._id) {
+         return wx.switchTab({ url: '/pages/message/message' });
+    }
 
     wx.showLoading({ title: '进入会话...' });
     
-    // 2. 查找我是否已经在这个房间里有过聊天记录
-    db.collection('chats').where({ 
-        roomId: this.roomId,
-        members: me._id 
-    }).get({
+    // 获取已入住成员
+    const roommates = this.data.info.memberIds || [];
+    const memberSet = new Set([owner._id, me._id, ...roommates]);
+    const allMembers = Array.from(memberSet);
+
+    db.collection('chats').where({ roomId: this.roomId, members: me._id }).get({
       success: res => {
         if (res.data.length > 0) {
           const chat = res.data[0];
-          
-          // 3. 这是一个已存在的会话，检查成员是否需要更新
-          // (例如：上次聊的时候还没室友，现在有室友了，要把室友拉进来)
-          // 简单的做法是：每次点击都更新一次 members 列表，确保是最新的
-          
-          db.collection('chats').doc(chat._id).update({
-             data: {
-                members: allMembers
-             }
-          }).then(() => {
-             // 甚至可以自动发一条系统消息提示新成员加入，这里先省略
-             this.navToChat(chat._id, this.data.info.community);
-          });
-          
+          db.collection('chats').doc(chat._id).update({ data: { members: allMembers } });
+          this.navToChat(chat._id, this.data.info.community);
         } else {
-          // 4. 我从没聊过，创建新群聊
           this.createChat(allMembers);
         }
-      },
-      fail: err => {
-          console.error(err);
-          wx.hideLoading();
-          wx.showToast({title: '网络异常', icon: 'none'});
       }
     });
   },
@@ -170,18 +189,13 @@ Page({
   createChat(members) {
     db.collection('chats').add({
       data: {
-        roomId: this.roomId, 
-        roomName: this.data.info.community + " 交流群",
+        roomId: this.roomId, roomName: this.data.info.community + " 交流群",
         members: members, 
-        // 初始未读：除了我之外的所有人
         unreadMembers: members.filter(id => id !== this.data.userInfo._id), 
-        lastMessage: '群聊已创建', 
-        updateTime: db.serverDate(),
+        lastMessage: '群聊已创建', updateTime: db.serverDate(),
         targetAvatar: this.data.info.cover || '' 
       },
-      success: res => { 
-          this.navToChat(res._id, this.data.info.community + " 交流群"); 
-      }
+      success: res => { this.navToChat(res._id, this.data.info.community + " 交流群"); }
     });
   },
 
@@ -190,42 +204,13 @@ Page({
     wx.navigateTo({ url: `/pages/chat/chat?id=${chatId}&name=${title}` });
   },
 
-  // --- 分享相关 ---
-  onShareAppMessage() {
-    const title = `【友邻找室友】${this.data.info.community} 招室友啦！`;
-    return {
-      title: title,
-      path: `/pages/detail/detail?id=${this.roomId}`,
-      imageUrl: this.data.info.cover
-    }
-  },
-  openShareModal() { this.setData({ showShareModal: true }); },
-  closeShareModal() { this.setData({ showShareModal: false }); },
-  handleShareTo(e) {
-    const appName = e.currentTarget.dataset.app;
-    const content = `【友邻】${this.data.info.community} 招室友！pages/detail/detail?id=${this.roomId}`;
-    wx.setClipboardData({
-      data: content,
-      success: () => {
-        this.closeShareModal();
-        wx.showModal({ title: '口令已复制', content: `请打开 ${appName} 粘贴`, showCancel: false });
-      }
-    });
-  },
-  handleCopyLink() {
-    wx.setClipboardData({
-      data: `pages/detail/detail?id=${this.roomId}`,
-      success: () => { this.closeShareModal(); wx.showToast({ title: '已复制' }); }
-    });
-  },
-
-  // --- 加入逻辑 ---
   openJoinModal() {
     if (!this.data.userInfo) return wx.navigateTo({ url: '/pages/login/login' });
     if (this.data.info.publisher._id === this.data.userInfo._id) return wx.showToast({title: '不能加入自己的房源', icon: 'none'});
     this.setData({ showJoinModal: true });
   },
   closeJoinModal() { this.setData({ showJoinModal: false }); },
+
   confirmJoin(e) {
     const { idx, name } = e.currentTarget.dataset;
     const me = this.data.userInfo;
